9844e57fbd68309c33d0cd0e260328216e0bce8b
rhonvyramos <rhonvyramos@gmail.com>
2023-12-24 08:42:11 -0500

    home element sizes set

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,11 @@ export default function Home() {
     return (
         <section id="page_home">
             <div id="home_contents">
-
+                <div id="home_margin">
+                    <div id="home_header" className="home_element"></div>
+                    <div id="home_main" className="home_element"></div>
+                    <div id="home_footer" className="home_element"></div>
+                </div>
             </div>
         </section>
     )
