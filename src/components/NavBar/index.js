e0a3289773bf86760a4462042bec06ad2af1cc6e
rhonvyramos <rhonvyramos@gmail.com>
2023-12-24 09:42:27 -0500

    browserrouter basic implementation; button link styling

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,15 +8,21 @@ export default function NavBar() {
     return (
         <section id="component_navbar">
             <div id="navbar_contents">
-                <div className="navbar_button">
-                    <p>About Me</p>
-                </div>
-                <div className="navbar_button">
-                    <p>Software Skills</p>
-                </div>
-                <div className="navbar_button">
-                    <p>Martial Arts Journey</p>
-                </div>
+                <Link to="/about" className="navbar_button">
+                    <div>
+                        <p>About Me</p>
+                    </div>
+                </Link>
+                <Link to="/software" className="navbar_button">
+                    <div>
+                        <p>Software Skills</p>
+                    </div>
+                </Link>
+                <Link to="/martialarts" className="navbar_button">
+                    <div>
+                        <p>Martial Arts Journey</p>
+                    </div>
+                </Link>
             </div>
         </section>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,16 +9,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function Home() {
     return (
-        <section id="page_home">
-            <div id="home_contents">
-                <div id="home_margin">
-                    <div id="home_header" className="home_element">
-                        <NavBar />
+        <Router>
+            <section id="page_home">
+                <div id="home_contents">
+                    <div id="home_margin">
+                        <div id="home_header" className="home_element">
+                            <NavBar />
+                        </div>
+                        <div id="home_main" className="home_element">
+                            <Routes>
+                                <Route path="about" element={<div>HOME</div>}/>
+                                <Route path="software" element={<div>SOFTWARE</div>}/>
+                                <Route path="martialarts" element={<div>MARTIAL ARTS</div>}/>
+                                <Route path="*" element={<div>NULL PAGE</div>}/>
+                            </Routes>
+                        </div>
+                        <div id="home_footer" className="home_element"></div>
                     </div>
-                    <div id="home_main" className="home_element"></div>
-                    <div id="home_footer" className="home_element"></div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </Router>
+
     )
 }
