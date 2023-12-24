0ea6df50ba15c26b9485e8dfc71db894c2898e0f
rhonvyramos <rhonvyramos@gmail.com>
2023-12-24 08:25:46 -0500

    page sizing done

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/home.css"
+
+export default function Home() {
+    return (
+        <section id="page_home">
+            <div id="home_contents">
+
+            </div>
+        </section>
+    )
+}
