e75624891ed41e76d6857d25a554178cdabbf3bc
rhonvyramos <rhonvyramos@gmail.com>
2023-12-25 13:48:38 -0500

    software page sizing set

--- src/pages/Software.js
+++ src/pages/Software.js
@@ -6,7 +6,22 @@ export default function Software() {
     return(
         <FadeIn inProp={true}>
             <section id="page_software">
-                SOFTWARE
+                <div id="software_contents">
+                    <div id="software_title">
+                        <p>Below are my most recent projects.</p>
+                    </div>
+                    <div id="software_projects">
+                        <div className="project_box">
+
+                        </div>
+                        <div className="project_box">
+
+                        </div>
+                        <div className="project_box">
+
+                        </div>
+                    </div>
+                </div>
             </section>
         </FadeIn>
     )
