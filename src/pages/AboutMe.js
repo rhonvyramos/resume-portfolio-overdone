9e70209157edff84f12febe10c3bc008efa34051
rhonvyramos <rhonvyramos@gmail.com>
2023-12-25 09:05:47 -0500

    about me images and text added

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,5 +1,12 @@
 import React from "react";
 import "../assets/aboutme.css"
+import rv_photo from "../assets/Ramos_RhonVincent.JPG"
+import html_icon from "../assets/html_icon.png";
+import css_icon from "../assets/css3_icon.png";
+import js_icon from "../assets/js_icon.png";
+import mysql_icon from "../assets/mysql_icon.png"
+import mongodb_icon from "../assets/mongodb_icon.png"
+import apollo_icon from "../assets/apollo_icon.png"
 
 import FadeIn from "../components/FadeIn";
 
@@ -7,7 +14,29 @@ export default function AboutMe() {
     return(
         <FadeIn inProp={true}>
             <section id="page_aboutme">
-                ABOUT ME
+                <div id="aboutme_contents">
+                    <div id="aboutme_images" className="aboutme_content">
+                        <div id="image_profile">
+                            <img src={rv_photo} alt="me" height={"250px"} width={"420px"} />
+                        </div>
+                        <div id="image_software">
+                            <img src={html_icon} alt="html" height={"60px"} width={"60px"} />
+                            <img src={css_icon} alt="css" height={"60px"} width={"60px"} />
+                            <img src={js_icon} alt="javascript" height={"60px"} width={"60px"} />
+                            <img src={mysql_icon} alt="mysql" height={"60px"} width={"60px"} />
+                            <img src={mongodb_icon} alt="mongodb" height={"60px"} width={"60px"} />
+                            <img src={apollo_icon} alt="apollo" height={"60px"} width={"60px"} />
+                        </div>
+                    </div>
+                    <div id="aboutme_text" className="aboutme_content">
+                        <div id="text_description">
+                            Welcome to my profile! 
+                            My name is Rhon Vincent Ramos and 
+                            I am a recent graduate of the George Washington 
+                            Full Stack Developer Coding Bootcamp!
+                        </div>
+                    </div>
+                </div>
             </section>
         </FadeIn>
     )
