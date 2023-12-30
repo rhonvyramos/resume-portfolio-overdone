4495ffb6b07d080a3cda56c7cd133f89ef3ac0d0
rhonvyramos <rhonvyramos@gmail.com>
2023-12-30 09:47:50 -0500

    updated resume link

--- src/components/ContactBar/index.js
+++ src/components/ContactBar/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import rvr_pdf from "../../assets/Resume_RVRamos.pdf"
 
 export default function Footer() {
     return (
@@ -9,7 +10,7 @@ export default function Footer() {
                     <a href="https://github.com/rhonvyramos" target="_blank" rel="noopener noreferrer">
                         <p>GitHub</p>
                     </a>
-                    <a href="https://drive.google.com/file/d/1OURUW3y9xB-5NeLxJnkfQdIOPco_jheM/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href={rvr_pdf} target="_blank" rel="noopener noreferrer">
                         <p>Resume</p>
                     </a>
                     <p>rhonvyramos@gmail.com</p>
